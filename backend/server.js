2c165bfd531eff73c1f2d7eabcd756e31211c6ac
thasup <thanachonfirst@hotmail.com>
2021-12-25 08:48:05 +0700

    change node syntax to ES6 module

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
