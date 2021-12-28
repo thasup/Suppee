830a80192341ef0722987279a2548a365fe05b99
thasup <thanachonfirst@hotmail.com>
2021-12-29 01:57:29 +0700

    Add addToCart function

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,24 +1,35 @@
 import { CART_ADD_ITEM } from "../constants/cartConstants";
 
+// search in the state.cartItems array
+// for the existence of the specific product
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
+            // that is defined by item (by the action.payload)
             const item = action.payload;
 
+            // if we find a match assign the result to the existItem variable
             const existItem = state.cartItems.find(
                 (x) => x.product === item.product
             );
 
+            // if there is already a product matching the item in the state.cartItems array
             if (existItem) {
+                // return the existing state with adjusted cartItems
                 return {
                     ...state,
+                    // map through the cardItems array and replace the matching product with the new item
+                    // leave the rest products as they were
                     cartItems: state.cartItems.map((x) =>
                         x.product === existItem.product ? item : x
                     ),
                 };
+                // otherwise if the item is NOT already a product matching the item in the state.cartItems array
             } else {
+                // return the existing state with adjusted cardItems
                 return {
                     ...state,
+                    // return a new cardItems array with the previous products spread and add the new item
                     cartItems: [...state.cartItems, item],
                 };
             }
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useLocation, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    Row,
+    Col,
+    ListGroup,
+    Image,
+    Form,
+    Button,
+    Card,
+} from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
 const CartScreen = () => {
+    const match = useParams();
+    const location = useLocation();
+    const productId = match.id;
+
+    const qty = new URLSearchParams(location.search).get("qty");
+
+    const dispatch = useDispatch();
+
+    const cart = useSelector((state) => state.cart);
+    const { cartItems } = cart;
+
+    console.log(cartItems);
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
+
     return <div>Cart</div>;
 };
 
