040eeadba07c26b094a8e371e6f3000cd78e1cf7
thasup <thanachonfirst@hotmail.com>
2021-12-29 14:46:02 +0700

    Add register route & password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,16 @@ userSchema.methods.matchPassword = async function (enterdPassword) {
     return await bcrypt.compare(enterdPassword, this.password);
 };
 
+userSchema.pre("save", async function (next) {
+    // Check if user profile data is updated or not, prevent it from regenerate new hash password
+    if (!this.isModified("password")) {
+        next();
+    } else {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
