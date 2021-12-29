150286e2071b482c1925747a8bd5ddc564be881c
thasup <thanachonfirst@hotmail.com>
2021-12-29 22:07:44 +0700

    Feat logout user

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,30 +1,60 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { logout } from "../actions/userActions";
 
 const Header = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  return (
-    <header>
-      <Navbar bg="primary" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand onClick={() => navigate("/")}>Suppee</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link onClick={() => navigate("/cart")}>
-                <i className="fas fa-shopping-cart"></i> Cart
-              </Nav.Link>
-              <Nav.Link onClick={() => navigate("/login")}>
-                <i className="fas fa-user"></i> Sign In
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+
+    const logoutHandler = () => {
+        dispatch(logout());
+    };
+
+    return (
+        <header>
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand onClick={() => navigate("/")}>
+                        Suppee
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link onClick={() => navigate("/cart")}>
+                                <i className="fas fa-shopping-cart"></i> Cart
+                            </Nav.Link>
+                            {userInfo ? (
+                                <NavDropdown
+                                    title={userInfo.name}
+                                    id="username"
+                                >
+                                    <Nav.Link
+                                        onClick={() => navigate("/profile")}
+                                    >
+                                        <NavDropdown.Item>
+                                            Profile
+                                        </NavDropdown.Item>
+                                    </Nav.Link>
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <Nav.Link onClick={() => navigate("/login")}>
+                                    <i className="fas fa-user"></i> Sign In
+                                </Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
 };
 
 export default Header;
