b24efe325e1aee380381bc4dea79cf02943944af
thasup <thanachonfirst@hotmail.com>
2021-12-31 05:45:19 +0700

    prepare for initial deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -21,13 +22,23 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
-
 app.get("/api/config/paypal", (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+const __dirname = path.resolve();
+
+if ((process.env.NODE_ENV = "production")) {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running...");
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
