ce26f3db157e697afca2587e4b0724809420f416
thasup <thanachonfirst@hotmail.com>
2022-01-04 23:17:50 +0700

    Add image upload endpoint & config

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -21,12 +22,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+// Mimic "__dirname" in ES Module nodeJS
 const __dirname = path.resolve();
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if ((process.env.NODE_ENV = "production")) {
     app.use(express.static(path.join(__dirname, "/frontend/build")));
